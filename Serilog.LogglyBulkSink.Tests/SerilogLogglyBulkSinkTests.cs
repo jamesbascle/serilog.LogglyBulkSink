5027f5a4474ed034679f0f024344f49650a006b3
jamesbascle <rbascle9@gmail.com>
2017-10-26 20:02:00 -0400

    changing to nunit, removing fluentassertions

--- Serilog.LogglyBulkSink.Tests/SerilogLogglyBulkSinkTests.cs
+++ Serilog.LogglyBulkSink.Tests/SerilogLogglyBulkSinkTests.cs
@@ -4,18 +4,17 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
-using FluentAssertions;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
+using NUnit.Framework;
 using Serilog.Events;
 using Serilog.Parsing;
 
 namespace Serilog.LogglyBulkSink.Tests
 {
-    [TestClass]
+    
     public class SerilogLogglyBulkSinkTests
     {
-        [TestMethod]
+        [Test]
         public void TestAddIfContains()
         {
             var dictionary = new Dictionary<string, string>()
@@ -23,16 +22,17 @@ namespace Serilog.LogglyBulkSink.Tests
                 {"hello", "world"}
             };
             LogglySink.AddIfNotContains(dictionary, "hello", "another world");
-            dictionary.ContainsKey("hello").Should().BeTrue();
-            dictionary["hello"].Should().Be("world");
+            Assert.IsTrue(dictionary.ContainsKey("hello"));
+            Assert.AreEqual(dictionary["hello"],"world");
+
 
 
             LogglySink.AddIfNotContains(dictionary, "newkey", "orange");
-            dictionary.ContainsKey("newkey").Should().BeTrue();
-            dictionary["newkey"].Should().Be("orange");
+            Assert.IsTrue(dictionary.ContainsKey("newkey"));
+            Assert.AreEqual(dictionary["newkey"], "orange");
         }
 
-        [TestMethod]
+        [Test]
         public void PackageContentsTest()
         {
             var jsons = new[]
@@ -45,15 +45,15 @@ namespace Serilog.LogglyBulkSink.Tests
             //changing to remove diagnostics parameter to show that the default version is false, and that this test ensures backwards API compatibility. Don't add it back w/o cutting major version!
             var noDiagContent = LogglySink.PackageContent(jsons, Encoding.UTF8.GetByteCount(string.Join("\n", jsons)), 0); 
             var stringContent = LogglySink.PackageContent(jsons, Encoding.UTF8.GetByteCount(string.Join("\n", jsons)), 0, true);
-            stringContent.Should().NotBeNull();
-            noDiagContent.Should().NotBeNull();
+            Assert.IsNotNull(stringContent);
+            Assert.IsNotNull(noDiagContent);
             var result = stringContent.ReadAsStringAsync().GetAwaiter().GetResult();
             var resultNoDiag = noDiagContent.ReadAsStringAsync().GetAwaiter().GetResult();
-            result.Split('\n').Length.Should().Be(4);
-            resultNoDiag.Split('\n').Length.Should().Be(3);
+            Assert.AreEqual(result.Split('\n').Length,4);
+            Assert.AreEqual(resultNoDiag.Split('\n').Length,3);
         }
 
-        [TestMethod]
+        [Test]
         public void TestRender()
         {
             var logEvent = new LogEvent(DateTimeOffset.UtcNow,
@@ -65,13 +65,13 @@ namespace Serilog.LogglyBulkSink.Tests
                 });
             var result = LogglySink.EventToJson(logEvent);
             var json = JsonConvert.DeserializeObject<dynamic>(result);
-            (json["test1"].Value as string).Should().Be("answer1");
+            Assert.AreEqual(json["test1"].Value as string, "answer1");
             bool hasZero = (json["0"] == null);
-            hasZero.Should().Be(true);
-            (json["key"].Value as string).Should().Be("value");
+            Assert.True(hasZero);
+            Assert.AreEqual(json["key"].Value as string,"value");
         }
 
-        [TestMethod]
+        [Test]
         public void IncludeDiagnostics_WhenEnabled_IncludesDiagnosticsEvent()
         {
             var logEvent = new LogEvent(DateTimeOffset.UtcNow,
@@ -87,12 +87,12 @@ namespace Serilog.LogglyBulkSink.Tests
             packageStringTask.Wait();
             var packageString = packageStringTask.Result;
 
-            (result.Count == 2).Should().BeTrue();
-            result[1].Contains("LogglyDiagnostics").Should().BeTrue();
-            packageString.Contains("LogglyDiagnostics").Should().BeTrue();
+            Assert.IsTrue(result.Count == 2);
+            Assert.IsTrue(result[1].Contains("LogglyDiagnostics"));
+            Assert.IsTrue(packageString.Contains("LogglyDiagnostics"));
         }
 
-        [TestMethod]
+        [Test]
         public void IncludeDiagnostics_WhenDisbled_DoesNotIncludeDiagnosticsEvent()
         {
             var logEvent = new LogEvent(DateTimeOffset.UtcNow,
@@ -108,12 +108,12 @@ namespace Serilog.LogglyBulkSink.Tests
             packageStringTask.Wait();
             var packageString = packageStringTask.Result;
 
-            (result.Count == 1).Should().BeTrue();
-            packageString.Contains("LogglyDiagnostics").Should().BeFalse();
+            Assert.IsTrue(result.Count == 1);
+            Assert.IsFalse(packageString.Contains("LogglyDiagnostics"));
         }
 
-        [TestMethod]
-        [TestCategory( "Integration")]
+        [Test]
+        [Category( "Integration")]
         public void WhenInvalidApiKeyProvided_OnSinkSend_TraceAndSerilogSelfLogPopulated()
         {
             var serilogSelfLogWriter = new StringWriter();
@@ -130,27 +130,23 @@ namespace Serilog.LogglyBulkSink.Tests
             logger.Dispose();
 
             var traceResult = traceWriter.ToString();
-            string.IsNullOrWhiteSpace(traceResult).Should().BeFalse();
+            Assert.IsFalse(string.IsNullOrWhiteSpace(traceResult));
 
             var selfLogResult = serilogSelfLogWriter.ToString();
-            string.IsNullOrWhiteSpace(selfLogResult).Should().BeFalse();
+            Assert.IsFalse(string.IsNullOrWhiteSpace(selfLogResult));
 
-            traceResult.Contains(
-                "Exception posting to loggly System.Net.Http.HttpRequestException: Response status code does not indicate success")
-                .Should()
-                .BeTrue();
+            Assert.IsTrue(traceResult.Contains(
+                "Exception posting to loggly System.Net.Http.HttpRequestException: Response status code does not indicate success"));
 
-            selfLogResult.Contains(
-                "Exception posting to loggly System.Net.Http.HttpRequestException: Response status code does not indicate success")
-                .Should()
-                .BeTrue();
+            Assert.IsTrue(selfLogResult.Contains(
+                "Exception posting to loggly System.Net.Http.HttpRequestException: Response status code does not indicate success"));
 
             Console.WriteLine(traceResult);
             Console.WriteLine(selfLogResult);
         }
 
-        [TestMethod]
-        [TestCategory("Integration")]
+        [Test]
+        [Category("Integration")]
         public void WhenInvalidApiKeyProvided_AndSelfLogOrTraceIsNotConfigured_EverythingIsOkay()
         {
             Trace.Listeners.Clear();
